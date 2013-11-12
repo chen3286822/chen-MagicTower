1df59ae67d1c3534b70a0fe60e607011d45508d8
chenfeifei <chen3286822@yeah.net>
2013-11-12 18:28:21 +0800

    继续添加优先攻击目标策略的AI

--- MagicTower/MagicTower/AIMgr.cpp
+++ MagicTower/MagicTower/AIMgr.cpp
@@ -2,6 +2,8 @@
 #include "Character.h"
 #include "CreatureManager.h"
 #include "MapManager.h"
+#include "ActionProcess.h"
+#include "ConfigManager.h"
 
 AIMgr g_AIMgr;
 
@@ -30,6 +32,85 @@ Character* AIMgr::GetCurCharacter()
 	return m_pCurAI;
 }
 
+int AIMgr::SelectDamageSkill(Character* target,POINT attackPoint)
+{
+	if(!m_pCurAI || !target)
+		return -1;
+
+	std::vector<int> skillList = m_pCurAI->GetSkillList();
+	int skillDefend = target->GetSkillDefend();
+	int expectedSkillDamage = 0;
+	int expectedSkill = -1;
+	for (std::vector<int>::iterator it=skillList.begin();it!=skillList.end();it++)
+	{
+		SkillInfo skill = ConfigManager::sInstance().GetSkillInfo().find(*it)->second;
+		//mp不足
+		if(m_pCurAI->GetMP() < skill->m_nCost)
+			continue;
+		//伤害型技能
+		if(skill->m_nSkillType == 1)
+		{
+			//判断是否可以攻击到
+			if(abs(target->GetBlock().xpos-attackPoint.x) + abs(target->GetBlock().ypos-attackPoint.y) <= skill.m_nCastRange)
+			{
+				int skillDamage = skill.m_nAttack - skillDefend;
+				if(skillDamage > expectedSkillDamage)
+				{
+					expectedSkill = skill.m_nID;
+					expectedSkillDamage = skillDamage;
+				}
+			}
+		}
+	}
+	return expectedSkill;
+}
+
+int AIMgr::GetDamageToTarget(Character* target,int& attack)
+{
+	if(!m_pCurAI || !target)
+		return 0;
+
+	int defend = target->GetDefend();
+	float dodge = target->GetDodge();
+	float crit = m_pCurAI->GetCrit();
+	int attack = m_pCurAI->GetAttack();
+	int skillDefend = target->GetSkillDefend();
+	//预期造成的物理伤害
+	int expectedDamage = (1-dodge)*(crit*(attack*2-defend) + (1-crit)*(attack-defend));
+	int skillID = SelectDamageSkill(target);
+	if(skillID == -1)
+	{
+		attack = -1;
+		return expectedDamage;
+	}
+	SkillInfo skill = ConfigManager::sInstance().GetSkillInfo().find(skillID)->second;
+	int skillDamage = skill.m_nAttack - skillDefend;
+	if(skillDamage > expectedDamage)
+	{
+		attack = skillID;
+		expectedDamage = skillDamage;
+	}
+	else
+		attack = -1;
+
+	return expectedDamage;
+}
+
+bool AIMgr::CanKillTarget(Character* target,int& attack)
+{
+	if(!m_pCurAI || !target)
+		return false;
+
+	int attackType = -1;
+	int expectedDamage = GetDamageToTarget(target,attackType);
+	if(expectedDamage >= target->GetHP())
+	{
+		attack = attackType;
+		return true;
+	}
+	return false;
+}
+
 bool AIMgr::DoAction()
 {
 	if(!m_pCurAI)
@@ -38,9 +119,9 @@ bool AIMgr::DoAction()
 	//根据策略确定行动
 	/*
 	*	如果是优先攻击目标策略（如果失败条件是己方全死，则目标单位就是列表中第一个单位）：
-	*	A、找出移动+攻击范围内所以可以攻击到的敌方单位，
+	*	A、找出移动+攻击范围内所有可以攻击到的敌方单位，
 	*	如果有目标单位，则攻击之(为了性能起见，这里攻击时判断如果能使用技能则使用最高伤害的手段)；否则判断是否有期望攻击致死
-	*	的单位，有则攻击之；否则攻击克制其兵种的单位；否则攻击血量最低单位
+	*	的单位，有则攻击之；否则攻击预期伤害最高的单位
 	*	B、找不到敌方单位，则选择向目标单位前进，前进后使用增益技能，没有则待命
 	*	如果是优先保护自己策略：
 	*	A、查找自己是否处于敌方目标攻击范围，且是否会被攻击致死(对于不同兵种，采取不同算法计算
@@ -100,6 +181,13 @@ bool AIMgr::DoAction()
 				m_pCurAI = NULL;
 				return true;
 			}
+			//攻击范围内没有敌方单位，选择向终极目标移动
+			if (targets.empty())
+			{
+				SelectRoute(ultraTarget);
+				return true;
+			}
+			//在所有可攻击敌人中发现终极目标
 			for (VAttackTarget::iterator it=targets.begin();it!=targets.end();it++)
 			{
 				if((*it).m_iTarget->GetNum() == ultraTarget->GetNum())
@@ -110,10 +198,70 @@ bool AIMgr::DoAction()
 					break;
 				}
 			}
+			ActionProcess* process = ActionProcess::sInstancePtr();
 			//如果可以直接攻击到终极目标的话
 			if(bUltraTarCanHit)
 			{
-
+				DWORD data = attackPoint.x + attackPoint.y << 8;
+				process->PushAction(eNotify_Walk,m_pCurAI,NULL,data);
+				CreatureManager::sInstance().PreAttackAndPushAction(m_pCurAI,ultraTarget);
+			}
+			else
+			{
+				int attackType;
+				//挑选可以直接击杀的单位
+				for (VAttackTarget::iterator it=targets.begin();it!=targets.end();it++)
+				{
+					attackType = -1;
+					if(CanKillTarget((*it).m_iTarget,attackType) == true)
+					{
+						bUltraTarCanHit = true;
+						attackPoint.x = (*it).m_iAttakPoint.x;
+						attackPoint.y = (*it).m_iAttakPoint.y;
+						ultraTarget = (*it).m_iTarget;
+						break;
+					}
+				}
+				//找到了
+				if(bUltraTarCanHit)
+				{
+					DWORD data = attackPoint.x + attackPoint.y << 8;
+					process->PushAction(eNotify_Walk,m_pCurAI,NULL,data);
+					if(attackType == -1)
+						CreatureManager::sInstance().PreAttackAndPushAction(m_pCurAI,ultraTarget);
+					else
+					{
+						m_pCurAI->GetCastSkill() = attackType;
+						CreatureManager::sInstance().PreSkillAndPushAction(m_pCurAI,ultraTarget);
+					}
+				}
+				//依旧没找到，则寻找预期造成伤害最高的
+				else
+				{
+					int attackType;
+					int expectedHigeDamage = 0;
+					for (VAttackTarget::iterator it=targets.begin();it!=targets.end();it++)
+					{
+						attackType = -1;
+						int expectedDamage = GetDamageToTarget((*it).m_iTarget,attackType);
+						if(expectedHigeDamage < expectedDamage)
+						{
+							expectedHigeDamage = expectedDamage;
+							attackPoint.x = (*it).m_iAttakPoint.x;
+							attackPoint.y = (*it).m_iAttakPoint.y;
+							ultraTarget = (*it).m_iTarget;
+						}
+					}
+					DWORD data = attackPoint.x + attackPoint.y << 8;
+					process->PushAction(eNotify_Walk,m_pCurAI,NULL,data);
+					if(attackType == -1)
+						CreatureManager::sInstance().PreAttackAndPushAction(m_pCurAI,ultraTarget);
+					else
+					{
+						m_pCurAI->GetCastSkill() = attackType;
+						CreatureManager::sInstance().PreSkillAndPushAction(m_pCurAI,ultraTarget);
+					}
+				}
 			}
 		}
 		break;
@@ -131,7 +279,7 @@ bool AIMgr::DoAction()
 	return true;
 }
 
-bool AIMgr::SelectRoute()
+bool AIMgr::SelectRoute(Character* target)
 {
 	if (!m_pCurAI)
 		return false;
